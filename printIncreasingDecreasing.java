34d5392399625abc2f0108232c5e6b747382b8d5
ADARSH PANDA <91756726+AdarshPanda931@users.noreply.github.com>
2025-03-04 22:51:16 +0530

    23rd program

---
+++ printIncreasingDecreasing.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class printIncreasingDecreasing {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        PID(n);
+    }
+    public static void PID(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        PID(n-1);
+        System.out.println(n);
+    }
+}
